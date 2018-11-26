7747c0102bab5056eadbe341596f37eeec2dac92
himkt <himkt@klis.tsukuba.ac.jp>
2018-11-26 10:36:27 +0900

    Add tests and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='pyner',
+      version='1.0',
+      description='Neural Named Entity Recognizer',
+      author='himkt',
+      author_email='himkt@klis.tsukuba.ac.jp',
+      url='https://github.com/himkt/pyner',
+      packages=find_packages())
