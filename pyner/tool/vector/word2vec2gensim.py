c4dfce00499dedf2df4378024ab66465563d05a9
himkt <himkt@klis.tsukuba.ac.jp>
2019-02-02 18:11:31 +0900

    Cleanup pyner/tool/vector

--- pyner/tool/vector/word2vec2gensim.py
+++ pyner/tool/vector/word2vec2gensim.py
@@ -6,21 +6,14 @@ import argparse
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input_file')
+    parser.add_argument('output_file')
     args = parser.parse_args()
 
     input_file = Path(args.input_file)
-    output_dir = Path('./data/processed', input_file.name)
-    output_dir.mkdir(exist_ok=True, parents=True)
+    output_file = Path(args.output_file)
 
     model = KeyedVectors.load_word2vec_format(args.input_file)
     print('loaded pre-trained word2vec model')
 
-    output_file = output_dir / 'model'
     model.save(output_file.as_posix())
     print('saved model')
-
-    vocabulary = sorted(list(model.wv.vocab.keys()))
-    vocabulary_path = output_dir / 'vocab.words.txt'
-    with open(vocabulary_path, 'w') as file:
-        for word in vocabulary:
-            print(word, file=file)
