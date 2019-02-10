3ce84c9e31383c00dd9c5b27312265519ea94831
himkt <himkt@klis.tsukuba.ac.jp>
2019-02-10 12:17:12 +0900

    Add a shell script to download a large file from gdrive

---
+++ data/processed/lample_embeddings/download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+filename="skip100.txt"
+file_id="0B23ji47zTOQNUXYzbUVhdDN2ZmM"
+query=`curl -c ./cookie.txt -s -L "https://drive.google.com/uc?export=download&id=${file_id}" \
+| perl -nE'say/uc-download-link.*? href="(.*?)\">/' \
+| sed -e 's/amp;//g' | sed -n 2p`
+url="https://drive.google.com$query"
+echo $url
+curl -b ./cookie.txt -L -o ${filename} $url
