7caaf00fee0993cd7183c997d5b2c3b8d7fef192
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2019-09-22 11:20:12 +0900

    Use pytest instead of unittest (#61)

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,6 +1,7 @@
-import unittest
-
-from pyner.tool.corpus.common import CorpusParser, bio2bioes, enum, iob2bio
+from pyner.tool.corpus.common import CorpusParser
+from pyner.tool.corpus.common import bio2bioes
+from pyner.tool.corpus.common import enum
+from pyner.tool.corpus.common import iob2bio
 
 TEST_CoNLL2003_1 = '''\
 -DOCSTART- -X- O O
@@ -21,36 +22,34 @@ TEST_BIO1 = ['B-PER', 'B-ORG', 'I-ORG', 'B-ORG', 'O']
 TEST_BIOES1 = ['S-PER', 'B-ORG', 'E-ORG', 'S-ORG', 'O']
 
 
-class TestEnumerate(unittest.TestCase):
-    def test_enum1(self):
-        word_sentences = [['今日', 'の', 'ごはん', 'は', 'カレー', 'カレー', '！！']]
-        tag_sentences = [['O', 'O', 'B-F', 'O', 'B-F']]
-        ws, cs, ts = enum(word_sentences, tag_sentences)
-        expected_ws = sorted(['今日', 'の', 'ごはん', 'は', 'カレー', '！！'])  # NOQA
-        expected_cs = sorted(list('今日のごはんカレー！'))
-        expected_ts = sorted(['O', 'B-F'])
+def test_enum1():
+    word_sentences = [['今日', 'の', 'ごはん', 'は', 'カレー', 'カレー', '！！']]
+    tag_sentences = [['O', 'O', 'B-F', 'O', 'B-F']]
+    ws, cs, ts = enum(word_sentences, tag_sentences)
+    expected_ws = sorted(['今日', 'の', 'ごはん', 'は', 'カレー', '！！'])  # NOQA
+    expected_cs = sorted(list('今日のごはんカレー！'))
+    expected_ts = sorted(['O', 'B-F'])
 
-        self.assertEqual(ws, expected_ws)
-        self.assertEqual(cs, expected_cs)
-        self.assertEqual(ts, expected_ts)
-
-
-class TestParser(unittest.TestCase):
-    def test_parse1(self):
-        parser = CorpusParser()
-        word_sentences, tag_sentences = \
-            parser._parse(TEST_CoNLL2003_1, word_idx=0, tag_idx=-1)
-        print(word_sentences)
-        self.assertEqual(word_sentences, [WORDS_CoNLL_1])
-        self.assertEqual(tag_sentences, [TAGS_CoNLL_1])
+    assert ws == expected_ws
+    assert cs == expected_cs
+    assert ts == expected_ts
 
 
-class TestTagSchemeConverter(unittest.TestCase):
-    def test_iob2bio1(self):
-        self.assertEqual(iob2bio(TEST_IOB1), TEST_BIO1)
+def test_parse1():
+    parser = CorpusParser()
+    word_sentences, tag_sentences = \
+        parser._parse(TEST_CoNLL2003_1, word_idx=0, tag_idx=-1)
+    assert word_sentences == [WORDS_CoNLL_1]
+    assert tag_sentences == [TAGS_CoNLL_1]
 
-    def test_bio2bioes1(self):
-        self.assertEqual(bio2bioes(iob2bio(TEST_BIO1)), TEST_BIOES1)
 
-    def test_iob2bioes1(self):
-        self.assertEqual(bio2bioes(iob2bio(TEST_IOB1)), TEST_BIOES1)
+def test_iob2bio1():
+    assert iob2bio(TEST_IOB1) == TEST_BIO1
+
+
+def test_bio2bioes1():
+    assert bio2bioes(iob2bio(TEST_BIO1)) == TEST_BIOES1
+
+
+def test_iob2bioes1():
+    assert bio2bioes(iob2bio(TEST_IOB1)) == TEST_BIOES1
--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -1,36 +1,35 @@
 from pyner.util.vocab import Vocabulary
 
-import unittest
 
-
-params1 = {'external': {'data_dir': './tests/test_data/v1'},
+PARAMS1 = {'external': {'data_dir': './tests/test_data/v1'},
            'preprocessing': {'replace_zero': True}}
 
-params2 = {'external': {'data_dir': './tests/test_data/v1'},
+PARAMS2 = {'external': {'data_dir': './tests/test_data/v1'},
            'preprocessing': {'lower': True}}
 
-params3 = {'external': {'data_dir': './tests/test_data/v1'},
+PARAMS3 = {'external': {'data_dir': './tests/test_data/v1'},
            'preprocessing': {'replace_zero': True, 'lower': True}}
 
 
-class VocabularyTest(unittest.TestCase):
-    def test_digit_normalize(self):
-        vocab = Vocabulary.prepare(params1)
-        result = vocab.vocab_arr['word']
-        expect = sorted(['apple00', 'wine', 'Apple00', 'Apple', 'apple'])
-        print('result: ', result)
-        self.assertEqual(expect, result)
+def test_digit_normalize():
+    vocab = Vocabulary.prepare(PARAMS1)
+    result = vocab.vocab_arr['word']
+    expect = sorted(['apple00', 'wine', 'Apple00', 'Apple', 'apple'])
+    print('result: ', result)
+    assert expect == result
 
-    def test_case_normalize(self):
-        vocab = Vocabulary.prepare(params2)
-        result = vocab.vocab_arr['word']
-        expect = sorted(['apple01', 'wine', 'apple02', 'apple'])
-        print('result: ', result)
-        self.assertEqual(expect, result)
 
-    def test_all_normalize(self):
-        vocab = Vocabulary.prepare(params3)
-        result = vocab.vocab_arr['word']
-        expect = sorted(['apple00', 'wine', 'apple'])
-        print('result: ', result)
-        self.assertEqual(expect, result)
+def test_case_normalize():
+    vocab = Vocabulary.prepare(PARAMS2)
+    result = vocab.vocab_arr['word']
+    expect = sorted(['apple01', 'wine', 'apple02', 'apple'])
+    print('result: ', result)
+    assert expect == result
+
+
+def test_all_normalize():
+    vocab = Vocabulary.prepare(PARAMS3)
+    result = vocab.vocab_arr['word']
+    expect = sorted(['apple00', 'wine', 'apple'])
+    print('result: ', result)
+    assert expect == result
