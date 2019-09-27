ad3b946ac60ffd800ea3eebde17151ebf17a9ee8
himkt <himkt@cookpad.com>
2019-09-27 12:05:55 +0000

    Replace pathlib with os.path and implement tagger

--- pyner/named_entity/tagger.py
+++ pyner/named_entity/tagger.py
@@ -1,25 +1,27 @@
 import json
 import logging
-import pathlib
 from typing import Optional
 
-import chainer
+import sys
 import click
+import chainer
+import os
 
+from tiny_tokenizer import WordTokenizer
 from pyner.named_entity.dataset import DatasetTransformer, converter
 from pyner.named_entity.recognizer import BiLSTM_CRF
 from pyner.util.deterministic import set_seed
-from pyner.util.iterator import create_iterator
 from pyner.util.metric import select_snapshot
 from pyner.util.vocab import Vocabulary
 
 
 @click.command()
-@click.argument("model")
+@click.argument("model_dir")
 @click.option("--epoch", type=int)
 @click.option("--device", type=int, default=-1)
 @click.option("--metric", type=str, default="validation/main/fscore")
-def run_inference(model: str, epoch: Optional[int], device: str, metric: str):
+def run_inference(
+        model_dir: str, epoch: Optional[int], device: str, metric: str):
     chainer.config.train = False
 
     if device >= 0:
@@ -27,53 +29,39 @@ def run_inference(model: str, epoch: Optional[int], device: str, metric: str):
 
     set_seed()
 
-    model_dir = pathlib.Path(model)
-    configs = json.load(open(model_dir / "args"))
-
-    snapshot_file, prediction_path = select_snapshot(
-        epoch, metric, model, model_dir)
+    config = json.load(open(os.path.join(model_dir, "args")))
+    snapshot_file, prediction_path = select_snapshot(epoch, metric, model_dir)
     logger.debug(f"creat prediction into {prediction_path}")
 
-    vocab = Vocabulary.prepare(configs)
-    num_word_vocab = configs["num_word_vocab"]
-    num_char_vocab = configs["num_char_vocab"]
-    num_tag_vocab = configs["num_tag_vocab"]
+    vocab = Vocabulary.prepare(config)
+    num_word_vocab = config["num_word_vocab"]
+    num_char_vocab = config["num_char_vocab"]
+    num_tag_vocab = config["num_tag_vocab"]
 
-    model = BiLSTM_CRF(configs, num_word_vocab, num_char_vocab, num_tag_vocab)
+    model = BiLSTM_CRF(config, num_word_vocab, num_char_vocab, num_tag_vocab)
 
-    model_path = model_dir / snapshot_file
+    model_path = os.path.join(model_dir, snapshot_file)
     logger.debug(f"load {snapshot_file}")
-    chainer.serializers.load_npz(model_path.as_posix(), model)
+    chainer.serializers.load_npz(model_path, model)
 
     if device >= 0:
         model.to_gpu(device)
 
     transformer = DatasetTransformer(vocab)
-    transform = transformer.transform
-    test_iterator = create_iterator(vocab, configs, "test", transform)
+    word_tokenizer = WordTokenizer("mecab")
 
-    with open(prediction_path, "w", encoding="utf-8") as file:
-        for batch in test_iterator:
-            in_arrays, t_arrays = converter(batch, device)
-            p_arrays = model.predict(in_arrays)
-
-            word_sentences, t_tag_sentences = list(
-                zip(*transformer.itransform(in_arrays[0], t_arrays))
-            )
-            _, p_tag_sentences = list(
-                zip(*transformer.itransform(in_arrays[0], p_arrays))
-            )
-
-            sentence_gen = zip(word_sentences, t_tag_sentences, p_tag_sentences)  # NOQA
-            for ws, ts, ps in sentence_gen:
-                for w, t, p in zip(ws, ts, ps):
-                    w = w.replace(" ", "<WHITESPACE>")
-                    print(f"{w} {t} {p}", file=file)
-                print(file=file)
+    for line in sys.stdin:
+        input_sentence = [str(t) for t in word_tokenizer.tokenize(line)]
+        batch = transformer.transform(input_sentence, None)
+        in_arr, _ = converter([batch])
+        pd_arr = model.predict(in_arr)
+        (_, tag_sequence), = transformer.itransform(in_arr[0], pd_arr)
+        print(' '.join(f"{word}/{tag}" for word, tag in zip(input_sentence, tag_sequence)))  # NOQA
 
 
 if __name__ == "__main__":
     logger = logging.getLogger(__name__)
     fmt = "%(asctime)s : %(threadName)s : %(levelname)s : %(message)s"
     logging.basicConfig(level=logging.DEBUG, format=fmt)
+    # pylint: disable=no-value-for-parameter
     run_inference()
